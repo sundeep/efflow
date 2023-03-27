1206b0a86655514342a8e0bbbab06bb98e993704
sundeep yedida <sundeep.yedida@reddit.com>
2023-03-26 17:05:25 -0700

    lint fix

--- efflow.py
+++ efflow.py
@@ -3,12 +3,18 @@ import requests
 API_URL = "https://api-inference.huggingface.co/models/microsoft/prophetnet-large-uncased-squad-qg"
 headers = {"Authorization": "Bearer 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"}
 
+
 def query(payload):
     response = requests.post(API_URL, headers=headers, json=payload)
     return response.json()
 
 
-txt = "I have a VW gti that needs to be sold. To get it ready for sale, I need to make sure it works and looks good. It is currently not starting, and looks pretty shitty. I don't know how much it would cost to get it ready for sale"
+txt = (
+    "I have a VW gti that needs to be sold.",
+    "To get it ready for sale, I need to make sure it works and looks good. ",
+    "It is currently not starting, and looks pretty shitty.",
+    "I don't know how much it would cost to get it ready for sale",
+)
 inny = {"inputs": txt}
 output = query(inny)
 
